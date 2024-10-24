88596a50d49684f4dd8afd0685cd883a5e0de2a8
Nahid0101 <mmmdovnahid27@gmail.com>
2024-10-25 01:07:16 +0400

    random

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
             System.out.println("i = " + i);
                    System.out.println("hello");
                    System.out.println("java codes");
+
+            System.out.println("random classsgit");
         }
     }
 }
